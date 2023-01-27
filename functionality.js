8f7aedc0c662493037b533a9a561172412001540
adekastanley <adekastanley2@gmail.com>
2023-01-27 18:53:34 +0100

    contact us done

--- functionality.js
+++ functionality.js
@@ -1,2 +1,23 @@
 const contactUS = document.querySelector('.market-form')
-console.log(contactUS)
+// console.log(contactUS.btn)
+
+contactUS.addEventListener('click', (e)=> {
+    e.preventDefault()
+    const name = contactUS.name.value
+    const email = contactUS.email.value
+    const phoneNumber = contactUS.phonenumber.value
+    const message = contactUS.note.value
+
+
+
+
+Email.send({
+    SecureToken : "43ef0c4b-c864-46c1-a467-36ce266b5836",
+    To : 'Smatradeng@gmail.com',
+    From : "Smatradeng@gmail.com",
+    Subject : "New Contact Request",
+    Body : `name ${name} ${email} phone number ${phoneNumber} message: ${message}  `  
+}).then(
+  message => console.log(message)
+);
+})
